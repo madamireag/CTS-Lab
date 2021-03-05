21dbfe598ea493788cb88523ab14383e27e96c90
Mireag Madalina <mireagmada@gmail.com>
2021-03-05 12:27:39 +0200

    added IoC

--- S2/src/ro/ase/acs/main/Main.java
+++ S2/src/ro/ase/acs/main/Main.java
@@ -1,8 +1,11 @@
 package ro.ase.acs.main;
 
 
+import ro.ase.acs.iocContainer.IoC;
 import ro.ase.acs.readers.ConsoleReader;
 import ro.ase.acs.writers.ConsoleWriter;
+import ro.ase.acs.writers.Writeable;
+import ro.ase.acs.readers.Readable;
 
 public class Main {
     public static void main(String[] args){
@@ -36,10 +39,19 @@ public class Main {
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
-        // Interface Segregation - daca as avea interfata IOCapable care face si read si write, sparg in
-        // interfetele Readable si Writeable
-        // Dependency Inversion
-        Orchestrator orchestrator = new Orchestrator(new ConsoleReader(),new ConsoleWriter());
+       // Interface Segregation - daca as avea interfata IOCapable care face si read si write, sparg in
+       // interfetele Readable si Writeable
+       // Dependency Inversion
+       // Orchestrator orchestrator = new Orchestrator(new ConsoleReader(),new ConsoleWriter());
+       // orchestrator.execute();
+
+        // IoC
+
+        IoC ioCContainer = new IoC();
+        ioCContainer.register(Readable.class, ConsoleReader.class);
+        ioCContainer.register(Writeable.class, ConsoleWriter.class);
+
+        Orchestrator orchestrator = new Orchestrator(ioCContainer);
         orchestrator.execute();
     }
 
